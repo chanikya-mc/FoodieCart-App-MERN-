4ea00e5ebd529e2dbd0b8a04cf09cd25746ff35b
M CHANIKYA <118601144+chanikya-mc@users.noreply.github.com>
2024-07-24 11:38:02 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -7,7 +7,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://foodiecart-app-mern-backend.onrender.com", {
+    const response = await fetch("https://foodiecart-app-mern-backend.onrender.com/api/auth/login", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
