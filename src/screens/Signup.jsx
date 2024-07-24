9604b426fc1984c35defb472cdf92e65c88a248c
M CHANIKYA <118601144+chanikya-mc@users.noreply.github.com>
2024-07-24 11:39:30 +0530

    Update Signup.jsx

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -21,7 +21,7 @@ export default function Signup() {
     // console.log(latlong)
     let [lat, long] = latlong
     console.log(lat, long)
-    const response = await fetch("https://foodiecart-app-mern-backend.onrender.com", {
+    const response = await fetch("https://foodiecart-app-mern-backend.onrender.com/api/auth/getlocation", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
